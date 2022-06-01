46e67511df62900412a31c7aa835dccc6a2fb3f9
prodavetc <prodavetc@gmail.com>
2022-05-31 22:10:04 -0300

    some fixing and add README

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 ## Short code generator
 
-for use call genValues(str, num, 4, true)
+for use call genValues(quantity, upper, alternation)
+
+* quantity of character <number>
+* uppcase for string <boolean> default false
+* alternation <boolean> default false (ex. d6f7)
+
--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
-const str ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-const num = '0123456789';
 
 const getCaracter = c => {
     return c.charAt(Math.floor(Math.random() * c.length));
-  }
+}
 
-
-const genValues = (str, num, quantity, upper = false, alternation = false) => {
-let result = ''
-  
-  
-  
-  for (let i = 1; i <= quantity; i++){
-    if(alternation){
-      if(i % 2  == 0){
-      	result += (getCaracter(num))
-      }else{
-        let up = (upper) ? getCaracter(str).toUpperCase() : getCaracter(str)
-        result += (up)
-      }
-    }else{
-      let res = str+=num
-      result += res.charAt(Math.floor(Math.random() * res.length));
-    }
+const genValues = (quantity, upper=false, alternation = false) => {
+    let str ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let num = '0123456789';
+    let result = ''
+  	let up = (upper) ? getCaracter(str).toUpperCase() : getCaracter(str)
     
-  }
+    for (let i = 1; i <= quantity; i++){
+        if(alternation){
+            if(i % 2  == 0){
+                result += getCaracter(num)
+            }else{
+                
+                result += (up)
+            }
+        }else{
+            let res = (up += num)
+            result += res.charAt(Math.floor(Math.random() * res.length));
+        }
+        
+    }
   return result
 }
 
-//genValues(str, num, 4, true)
 module.exports = {
     genValues
 }
